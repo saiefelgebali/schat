09ba6645f0a04a17476cdf41173e2785264861d3
Saief El Gebali <saiefelsayed@gmail.com>
2022-10-04 11:15:25 +0100

    REFACTOR: Server

---
+++ server/build/log.js
@@ -0,0 +1,29 @@
+export var ConsoleColor;
+(function (ConsoleColor) {
+    ConsoleColor["Reset"] = "\u001B[0m";
+    ConsoleColor["Bright"] = "\u001B[1m";
+    ConsoleColor["Dim"] = "\u001B[2m";
+    ConsoleColor["Underscore"] = "\u001B[4m";
+    ConsoleColor["Blink"] = "\u001B[5m";
+    ConsoleColor["Reverse"] = "\u001B[7m";
+    ConsoleColor["Hidden"] = "\u001B[8m";
+    ConsoleColor["FgBlack"] = "\u001B[30m";
+    ConsoleColor["FgRed"] = "\u001B[31m";
+    ConsoleColor["FgGreen"] = "\u001B[32m";
+    ConsoleColor["FgYellow"] = "\u001B[33m";
+    ConsoleColor["FgBlue"] = "\u001B[34m";
+    ConsoleColor["FgMagenta"] = "\u001B[35m";
+    ConsoleColor["FgCyan"] = "\u001B[36m";
+    ConsoleColor["FgWhite"] = "\u001B[37m";
+    ConsoleColor["BgBlack"] = "\u001B[40m";
+    ConsoleColor["BgRed"] = "\u001B[41m";
+    ConsoleColor["BgGreen"] = "\u001B[42m";
+    ConsoleColor["BgYellow"] = "\u001B[43m";
+    ConsoleColor["BgBlue"] = "\u001B[44m";
+    ConsoleColor["BgMagenta"] = "\u001B[45m";
+    ConsoleColor["BgCyan"] = "\u001B[46m";
+    ConsoleColor["BgWhite"] = "\u001B[47m";
+})(ConsoleColor || (ConsoleColor = {}));
+export const log = (message, options) => {
+    console.log(`${options?.color ?? ''}[socket.ts]${ConsoleColor.Reset} ${message}`);
+};
--- server/build/socket.js
+++ server/build/socket.js
@@ -1,49 +1,62 @@
+import { ConsoleColor, log } from './log.js';
 import db from '../../shared/db.js';
+/** Standardize every user's friend group label */
+export const friendsOfUserRoom = (username) => `friends-of-${username}`;
 export const configureSocket = (io) => {
-	// Keep track of who is online
-	const userSockets = {};
-	io.on('connection', (socket) => {
-		let username = '';
-		socket.on('join', async (message) => {
-			username = message.username;
-			userSockets[username] = socket;
-			// Alert friends
-			const user = await db.user.findUnique({ select: { friends: true }, where: { username } });
-			if (!user) return;
-			user.friends.forEach((f) => {
-				if (!(f.username in userSockets)) return;
-				userSockets[f.username].emit('online', { username, status: true });
-				// Alert self of online friends
-				socket.emit('online', { username: f.username, status: true });
-			});
-		});
-		socket.on('message', (message) => {
-			if (message.to in userSockets) {
-				const friend = userSockets[message.to];
-				friend.emit('message', message);
-			}
-			socket.emit('message', message);
-		});
-		socket.on('typing', (message) => {
-			if (!(message.to in userSockets)) return;
-			const friend = userSockets[message.to];
-			friend.emit('typing', {
-				from: message.from,
-				status: message.status
-			});
-		});
-		socket.on('disconnect', async () => {
-			delete userSockets[username];
-			// Alert friends
-			const user = await db.user.findUnique({
-				select: { friends: true },
-				where: { username }
-			});
-			if (!user) return;
-			user.friends.forEach((f) => {
-				if (!(f.username in userSockets)) return;
-				userSockets[f.username].emit('online', { username, status: false });
-			});
-		});
-	});
+    // Keep track of who is online
+    const userSockets = {};
+    io.on('connection', (socket) => {
+        let username = '';
+        log('New socket connection');
+        socket.on('join', async (message) => {
+            username = message.username;
+            log(`'${username}' joined.`, { color: ConsoleColor.FgCyan });
+            // Invalid join
+            if (!username) {
+                log('ERROR: socket was disconnected forcefully.', { color: ConsoleColor.FgRed });
+                return socket.disconnect(true);
+            }
+            userSockets[username] = socket;
+            // Add to friends' rooms
+            const user = await db.user.findUnique({ select: { friends: true }, where: { username } });
+            user.friends.forEach((friend) => {
+                // Add to friends' groups
+                socket.join(friendsOfUserRoom(friend.username));
+                // Make sure friend is in my room
+                userSockets[friend.username]?.join(friendsOfUserRoom(username));
+                // Update users already connected
+                socket.emit('online', {
+                    username: friend.username,
+                    status: friend.username in userSockets
+                });
+            });
+            // Notify friends that this user joined
+            io.to(friendsOfUserRoom(username)).emit('online', {
+                username,
+                status: true
+            });
+        });
+        socket.on('message', (message) => {
+            if (message.to in userSockets) {
+                const friend = userSockets[message.to];
+                friend.emit('message', message);
+            }
+            socket.emit('message', message);
+        });
+        socket.on('typing', (message) => {
+            if (!(message.to in userSockets))
+                return;
+            const friend = userSockets[message.to];
+            friend.emit('typing', {
+                from: message.from,
+                status: message.status
+            });
+        });
+        socket.on('disconnect', async () => {
+            delete userSockets[username];
+            log(`'${username}' disconnected.`, { color: ConsoleColor.FgYellow });
+            // Notify friends that this user disconnected
+            io.to(friendsOfUserRoom(username)).emit('online', { username, status: false });
+        });
+    });
 };
